d979ecf57d56c698ad5adaf55f3fe830fe15aad5
Saul <saulplascencia09@gmail.com>
2026-02-12 20:58:27 -0800

    practice

--- Queue-1/Queue.hs
+++
@@ -1,8 +0,0 @@
--- Simple Queue Interface
-
-module Queue (Queue(..)) where
-
-class Queue q where
-  mtq  :: q a                    -- empty queue
-  addq :: a -> q a -> q a        -- add element to back of queue
-  remq :: q a -> Maybe (a, q a)  -- remove element from front of queue
